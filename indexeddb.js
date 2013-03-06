77234e1353effe91675eb93009cc6f66142b1ba8
Patrick Golden <ptgolden0@gmail.com>
2013-03-05 16:08:50 -0800

    Make indexeddb backend match the main page

--- indexeddb.js
+++ indexeddb.js
@@ -1,101 +1,78 @@
-var indexedDB
-  , IDBKeyRange
-
-indexedDB = window.indexedDB
-  || window.mozIndexedDB
-  || window.webkitIndexedDB
-  || window.msIndexedDB;
-IDBKeyRange = window.IDBKeyRange
-  || window.webkitIDBKeyRange
-  || window.msIDBKeyRange;
-
-function dbCreated() {
-  reportAction('DB created');
-  $('#delete-db, #load-data').prop('disabled', false);
-  $('#open-db').prop('disabled', true);
-  return;
-}
-
-function dbDeleted() {
-  reportAction('DB deleted');
-  $('#delete-db, #load-data, #textinput').prop('disabled', true);
-  $('#open-db').prop('disabled', false);
-  return;
-}
-
 var IDBBackend = function () {
-  var that = this
+  var backend = this
+    , indexedDB
+    , IDBKeyRange
+
+  indexedDB = window.indexedDB
+    || window.mozIndexedDB
+    || window.webkitIndexedDB
+    || window.msIndexedDB;
+  IDBKeyRange = window.IDBKeyRange
+    || window.webkitIDBKeyRange
+    || window.msIDBKeyRange;
+
+  function createObjectStore(db, source) {
+    var objectStore = db.createObjectStore(source, {
+      keyPath: 'id',
+      autoIncrement: true
+    });
+    objectStore.createIndex('keywords', 'keywords', {
+      unique: false,
+      multiEntry: true
+    });
+  }
 
   this.db = null;
+  this.oninit = undefined;
+  this.ondestroyed = undefined;
+  this.destroyed = false;
 
   this.init = function () {
-    var req;
-
-    req = indexedDB.open('idb_test');
+    var req = indexedDB.open('idb_test');
     req.onsuccess = function (e) {
-      that.db = this.result;
-      dbCreated();
+      backend.db = this.result;
+      if (backend.oninit) {
+        backend.oninit.apply(backend);
+      }
     }
     req.onupgradeneeded = function (e) {
-      var db = this.result
-        , topicsObjectStore
-
-      topicsObjectStore = db.createObjectStore('topics', {
-        keyPath: 'id',
-        autoIncrement: true
-      });
-      topicsObjectStore.createIndex('keywords', 'keywords', {
-        unique: false,
-        multiEntry: true
-      });
+      var db = this.result;
+      for (var source in dataSources) {
+        createObjectStore(db, source);
+      }
     }
     req.onerror = function (e) {
       console.log('Database error', e);
     }
   }
 
-  this.loadData = function (name) {
-    var dataSource = dataSources[name]
-      , req
-      , db = this.db
+  this.loadData = function (source) {
+    var db = this.db
+      , req = new XMLHttpRequest()
+      , dataSource = dataSources[source]
 
     if (!dataSource) {
-      console.log('no such data source: ' + name)
+      console.log('no such data source: ' + source)
       return;
     }
 
-    req = new XMLHttpRequest();
     req.onload = function () {
       var data = JSON.parse(this.responseText)
-        , startTime = Date.now()
-        , endTime
-        , transaction
-        , objectStore
+        , start = Date.now()
+        , transaction = db.transaction([source], 'readwrite')
+        , objectStore = transaction.objectStore(source)
 
-        trans = transaction
-
-      transaction = db.transaction([name], 'readwrite')
       transaction.oncomplete = function () {
-        endTime = Date.now();
-        reportAction('loaded ' + dataSource.src 
-            + ' (' + data[name].length + ' records)', startTime, endTime);
-        $('#textinput')
-          .prop('disabled', false)
-          .off('**')
-          .on('input', function () {
-            that.performSearch(this.value);
-          });
+        var msg = 'Loaded ' + data.items.length + ' records from ' + dataSource.src;
+        reportAction(msg, start, Date.now());
+        enableSearch();
       }
-      objectStore = transaction.objectStore(name);
 
-      data[name].forEach(function (item) {
-        var keywords = [];
-        dataSource.keyword_fields.forEach(function (field) {
-          [].concat(item[field]).forEach(function (phrase) {
-            keywords = keywords.concat(getKeywords(phrase));
-          })
+      data.items.forEach(function (item) {
+        kwFields = dataSource.keyword_fields.map(function (field) {
+          return item[field];
         });
-        item.keywords = keywords;
+        item.keywords = buildKeywords(kwFields);
         objectStore.put(item);
       });
 
@@ -105,17 +82,15 @@ var IDBBackend = function () {
     return;
   }
 
-  this.performSearch = function (phrase) {
+  this.performSearch = function (source, phrase) {
     var start = Date.now()
       , $container = $('#results').html('')
       , firstWord = phrase.split(' ')[0].toLowerCase()
       , range
       , transaction
+      , counter = 0
 
     if (!firstWord || firstWord.length < 2) {
-      if (that.currentTransaction) {
-        that.currentTransaction.abort();
-      }
       return;
     }
 
@@ -123,8 +98,8 @@ var IDBBackend = function () {
     // word of the searched phrase.
     range = IDBKeyRange.bound(firstWord, firstWord + '\uffff');
 
-    transaction = that.db.transaction(['topics']);
-    that.currentTransaction = transaction;
+    transaction = backend.db.transaction(['topics']);
+    backend.currentTransaction = transaction;
 
     transaction
       .objectStore('topics')
@@ -132,23 +107,27 @@ var IDBBackend = function () {
       .openCursor(range)
       .onsuccess = function (e) {
         var cursor = e.target.result;
-        if (cursor && that.currentTransaction === transaction) {
+        if (cursor && backend.currentTransaction === transaction) {
           $container.append('<div>' + cursor.value.name + '</div>');
+          counter += 1;
           cursor.continue();
         } else if (!cursor) {
-          var end = Date.now();
-          reportAction('Searched for ' + phrase, start, end);
-          that.currentTransaction = null;
+          var end = Date.now(), msg;
+          msg = counter + ' results for "' + phrase + '" in ' + (end - start) + 'ms';
+          $container.prepend('<p><strong>' + msg + '</strong></p>');
+          backend.currentTransaction = null;
         }
       }
   }
 
   this.teardown = function () {
     var req = indexedDB.deleteDatabase('idb_test').onsuccess = function () {
-      dbDeleted();
+      if (backend.onteardown) {
+        backend.onteardown.apply(backend);
+      }
     }
-    if (that.db) {
-      that.db.close();
+    if (backend.db) {
+      backend.db.close();
     }
   }
 
